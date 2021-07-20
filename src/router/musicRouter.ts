8c78ef4ab11b792fcaeb9d6097e4eba76b3e5081
Janis Costadelli <77967942+janiscostadelli@users.noreply.github.com>
2021-07-20 09:51:44 -0300

    Update musicRouter.ts

--- src/router/musicRouter.ts
+++ src/router/musicRouter.ts
@@ -3,5 +3,5 @@ import MusicController from "../controller/MusicController";
 
 export const musicRouter = express.Router();
 musicRouter.post("/create", MusicController.createMusic);
+musicRouter.post("/search", MusicController.searchMusic);
 musicRouter.get("/all", MusicController.getAllMusics);
-musicRouter.post("/:playlistId", MusicController.searchMusic);
